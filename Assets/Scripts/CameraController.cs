4ac569911867bd81f677e9e924940760d6a0b744
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2022-01-13 23:16:47 -0300

    Cambios esteticos

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,24 +4,62 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float Sensibilidad;
+     public float Sensibilidad;
 
-    public Vector3 Distancia;
-    private Transform Player;
-    [Range(0, 1)] public float ValorLerp; //Cuan rapido cambia de posicion la camara
+     public Vector3 Distancia;
+     private Transform Player;
+     [Range(0, 1)] public float ValorLerp; //Cuan rapido cambia de posicion la camara
 
-    // Start is called before the first frame update
-    void Start()
+     // Start is called before the first frame update
+     void Start()
+     {
+         Player = GameObject.Find("Ellen").transform;
+     }
+
+     // Update is called once per frame
+     void LateUpdate()
+     {
+         transform.position = Vector3.Lerp(transform.position, Player.position + Distancia, ValorLerp);
+         Distancia = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * Sensibilidad, Vector3.up) * Distancia;
+
+         transform.LookAt(Player); //Acomoda el angulo automaticamente
+     }
+
+
+   /* public float cameraSmoothingFactor = 1;
+    public float lookUpMax = 60;
+    public float lookDownMax = -60;
+
+    public Transform cameratransform;
+
+    private Quaternion camRotation;
+
+    private void Start()
     {
-        Player = GameObject.Find("Ellen").transform;
+        camRotation = transform.localRotation;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    private void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, Player.position + Distancia, ValorLerp);
-        Distancia = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * Sensibilidad, Vector3.up) * Distancia;
+        camRotation.x += Input.GetAxis("Mouse Y") * cameraSmoothingFactor * (-1);
+        camRotation.y += Input.GetAxis("Mouse X") * cameraSmoothingFactor;
 
-        transform.LookAt(Player); //Acomoda el angulo automaticamente
+        camRotation.x = Mathf.Clamp(camRotation.x, lookDownMax, lookUpMax);
+
+        transform.localRotation = Quaternion.Euler(camRotation.x, camRotation.y, camRotation.z);
+
+
     }
+    */
+
+
+
+
+
+
+
+
+
+
+
 }
